f8bab1e712d2545bf96373a645831a212e5a0e5c
Felix Fernando <felixzhuang1@gmail.com>
2021-09-20 22:37:24 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Hero />
+    <Large-card-display 
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+    <Small-card-display 
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardSection="cardInfo"
+    />
+  </div>
+  
+</template>
+
+<script>
+  import { largeCardSections, smallCardSections } from "@/static/data.js"
+    export default {
+        data() {
+            return {
+              largeCardInfo: largeCardSections,
+              smallCardInfo: smallCardSections
+            }
+        }
+    }
+</script>
